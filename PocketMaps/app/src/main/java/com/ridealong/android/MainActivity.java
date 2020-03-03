819df18f6c9b4871ffb3a75d674c1899fce790ad
Goofynose <58679893+Goofynose@users.noreply.github.com>
2020-03-02 17:56:47 -0800

    Added placeholder login screen

--- PocketMaps/app/src/main/java/com/ridealong/android/MainActivity.java
+++ PocketMaps/app/src/main/java/com/ridealong/android/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ridealong.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.junjunguo.pocketmaps.R;
 import com.junjunguo.pocketmaps.activities.PocketMainActivity;
+import com.ridealong.android.ui.login.LoginActivity;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +20,17 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PocketMainActivity activity = new PocketMainActivity();
+
+        Intent login = new Intent(this, LoginActivity.class);
+
+        startActivityForResult(login, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Intent pocketmaps = new Intent(this, PocketMainActivity.class);
+        startActivity(pocketmaps);
     }
 
 }
